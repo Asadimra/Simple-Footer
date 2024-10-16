ef9857c66d1b0d9fa38363a0f5720cdc6effa9cc
Asadimra <asadimran031@gmail.com>
2024-10-16 21:43:04 +0500

    upload a footer

---
+++ app/component/footer.tsx
@@ -0,0 +1,41 @@
+
+ export default function Footer(){
+    return(
+   <div>
+    <footer>
+        <div className="container">
+            <div className="box1">
+                <ul>
+                    <li>Contact</li>
+                    <li>Our Service</li>
+                    <li>Privacy Policy</li>
+                    <li>Affiliate Program</li>
+                </ul>
+            </div>
+                <div className="box2">
+                <ul>
+                    <li>FAQ</li>
+                    <li>Shipping</li>
+                    <li>Return</li>
+                    <li>Order Status</li>
+                </ul>
+        </div>
+                <div className="box3">
+                    <ul>
+                        <li>Watch</li>
+                        <li>Bag</li>
+                        <li>Shoes</li>
+                        <li>Dress</li>
+                    </ul>
+                </div>
+
+            </div>
+        
+
+    </footer>
+        
+    </div>
+    
+    )
+
+}
